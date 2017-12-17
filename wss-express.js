89d43e3eecfeca9585ead1b6098d65500bb7c6bd
JamesXun <xjf_pro@163.com>
2017-12-17 19:16:59 +0800

    使用ws后，完善聊天室逻辑，待调试

--- wss-express.js
+++ wss-express.js
@@ -9,8 +9,8 @@ var server = https.createServer({
 	'passphrase': 'ffxffx'
 });
 var url = require('url');
-var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({
+var WebSocket = require('ws');
+var wss = new WebSocket.Server({
 	server: server
 });
 var express = require('express');
@@ -21,42 +21,76 @@ app.use(function (req, res) {
 		msg: "hello"
 	});
 });
-
+// 上传聊天记录
+wss.uploadMsg = function (msg) {
+	const { uid, roomId, ip } = this
+    let groupId = 0,
+    	toUid = 0;
+    global.Dubbo.Chat
+      .addMessage(uid, msg, ip, parseInt(roomId), groupId, toUid)
+      .then(data => {
+		// res.json(returnFmt.normal(JSON.parse(data)))
+		console.log(`uid:${uid}的消息上传成功`)
+      })
+      .catch(err => {
+		console.log(`uid: ${uid}的消息上传失败，原因: ${err}`)
+        // res.json(returnFmt.error(err))
+      });
+}
+// 获取房间历史记录
+wss.getHistory = function (ws, type) {
+	global.Dubbo.Chat
+		.getNewMsg(parseInt(type))
+		.then(data => {
+			data = JSON.parse(data)
+			// 数据的过滤与组合
+			data.data.forEach(x => {
+				if (x.uptime && typeof x.uptime === 'string') {
+				x.uptime = moment(new Date(x.uptime)).format('YYYY-MM-DD HH:mm:ss')
+				}
+			})
+			// 返回所需数据
+			ws.send(JSON.stringify({
+				state: 1,
+				data: data.data
+			}))
+		})
+		.catch(err => {
+			// res.json(returnFmt.error(err))
+			ws.send(JSON.stringify({state: 0}))
+		});
+}
 wss.on('connection', function connection(ws, req) {
-	const location = url.parse(req.url, true)
-	// you might use location.query.access_token to authenticate or share sessions
-	// or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-	// ws.send(JSON.stringify(ws))
+	const ip = req.connection.remoteAddress
+	// const ip = req.headers['x-forwarded-for'] // 使用NGINX时候获取ip方法
+	const query = url.parse(req.url, true).query // 解析地址 获取uid roomId等
+	let { roomId, uid } = query
+
+	// 绑定WebSocketServer对象(包含各种数据)
+	Object.assign(wss, { roomId, uid, ip })
+	ws.wss = wss
+
+	// 获取历史记录
+	ws.wss.getHistory(ws, 1)
+
+	// 监听聊天
 	ws.on('message', function incoming(msg) {
 		console.log('received: %s', msg)
-		// ws.send(msg)
 		this.wss.broadcast(msg, roomId)
+		this.wss.uploadMsg(msg)
 	})
-
-	// 识别成功，把user绑定到该WebSocket对象:
-    // ws.user = user;
-    // 绑定WebSocketServer对象:
-	ws.wss = wss
-	ws.roomId = 
-});
+})
 // 广播方法
 wss.broadcast = function (msg, roomId) {
-	let str = ''
     wss.clients.forEach(client => {
-		// for (const key in client) {
-		// 	if (client.hasOwnProperty(key)) {
-		// 		// const val = client[key]
-		// 		console.log(key)
-		// 	}
-		// }
-		if (client.roomId === roomId) {
+		if (client.readyState === WebSocket.OPEN && client.roomId === roomId) {
 			client.send(msg)
 			console.log(`room: ${roomId}广播消息 ${msg}`)
 		}
-    });
-};
+    })
+}
 
 server.on('request', app);
 server.listen(port, function () {
 	console.log('Listening on ' + server.address().port)
-});
+})
